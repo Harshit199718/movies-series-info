ec320a82831091977192f6db4e80581222410655
Harshit <harshitupadhyay872@gmail.com>
2020-04-07 13:44:11 +0530

    Updates

---
+++ precache-manifest.9fe96e7bbe4d491587bcdad25a27e80f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04689a9c5afd6aadc41684013af31b75",
+    "url": "/movies-series-info/index.html"
+  },
+  {
+    "revision": "4c88988928b5370a2866",
+    "url": "/movies-series-info/static/js/2.9f3fdaf7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies-series-info/static/js/2.9f3fdaf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c07d65bf633f2c7aa7ed",
+    "url": "/movies-series-info/static/js/main.84da0683.chunk.js"
+  },
+  {
+    "revision": "fff5a707fbd64b8efe64",
+    "url": "/movies-series-info/static/js/runtime-main.ec3583f3.js"
+  }
+]);
